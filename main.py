0d1b618679f8973e5a04484e7b8c2d5f84e4b0ff
Zach Holder <zacharysholder@gmail.com>
2018-10-09 22:11:12 -0500

    added post method for username

--- main.py
+++ main.py
@@ -9,4 +9,9 @@ app.config['DEBUG'] = True
 def index():
     return render_template("form.html")
 
+@app.route("/", methods=['POST'])
+def validate():
+    username = request.form['username']
+    return render_template("success.html", username=username)
+
 app.run()
